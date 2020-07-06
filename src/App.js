2a4f31eaa733726daea20851019d63c4b34bf05a
vexild <vili.ahonen@hotmail.com>
2020-07-06 13:05:07 +0300

    added commonheader.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Product from './Product'
+import CommonHeader from './Components/commonHeader'
 
 function App() {
   return (
-    <Router>
-     <div>
-       <Link to="/">Etusivu </Link>
-       <Link to="/tuotteet">Tuotteet</Link>
+    <div>
+      <CommonHeader/>
+      <Router>
+      <div>
+        <Link to="/">Etusivu </Link>
+        <Link to="/tuotteet">Tuotteet</Link>
 
-       <Route exact path="/" render={(props) => (
-         <p>Front</p>
-       )} />
-       <Route path="/tuotteet" render={(props) => (
-         <Product /> 
-       )} />
+        <Route exact path="/" render={(props) => (
+          <p>Front</p>
+          )} />
+        <Route path="/tuotteet" render={(props) => (
+          <Product /> 
+          )} />
 
-       </div> 
-    </Router>
+        </div> 
+      </Router>
+    </div>
   )
 }
 
---
+++ src/Components/commonHeader.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import headerStyling from '../Styling/headerStyling'
+
+
+const CommonHeader = () => {
+
+    return (
+        <div >
+            <div className="header-grey-background">
+                
+            </div>
+            <div>
+                <h1>CAFE sample</h1>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default CommonHeader;
+
