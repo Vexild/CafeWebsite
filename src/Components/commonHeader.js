b06261154b3327843dedd646ef3791e04eaa6b6f
vexild <vili.ahonen@hotmail.com>
2020-07-07 09:10:48 +0300

    Added primitive header. More to be come

--- src/Components/commonHeader.js
+++ src/Components/commonHeader.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import headerStyling from '../Styling/headerStyling'
-
+// eslint-disable-next-line no-unused-vars
+import '../Styling/header.css'
+import IGIcon from '../Icons/ig_icon_color.png'
+import FBIcon from '../Icons/fb_icon_color.png'
 
 const CommonHeader = () => {
 
     return (
         <div >
             <div className="header-grey-background">
+                <h1 className="sample-font">CAFE sample</h1>
+                <img  className="header-icon-background" src={IGIcon} alt="IGIcon"/>
+                <img  className="header-icon-background" src={FBIcon} alt="IGIcon"/>
                 
-            </div>
-            <div>
-                <h1>CAFE sample</h1>
-
             </div>
         </div>
 
