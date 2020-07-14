15660c42ffded2f9d421621b9af1906b52e5d808
jyrimy <jyrimy@gmail.com>
2020-07-14 10:00:39 +0300

    Some Data declarations

--- src/Components/panelCanvas.js
+++ src/Components/panelCanvas.js
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
 import '../Styling/panelCanvas.css';
 
 const panelCanvas = () => {
+    const [cafeDescription, setCafeDescription] = ({})
+    const [topicalText, setTopicalText] = ({})
+
     return (
         <div>
             <Container>
-                <Row><Col className="mainDescription">Höpönlöpön pöppöö</Col></Row>
+                <Row><Col className="cafeDescription">Höpönlöpön pöppöö</Col></Row>
                 <Row>
                     <Col className="panel"><p>Yo</p></Col>
                     <Col className="panel"><p>tsiga</p></Col>
