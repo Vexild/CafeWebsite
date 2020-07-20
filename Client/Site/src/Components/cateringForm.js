ef27e92769b2670b1679e38ef0ed8ec380aa4ac3
birghit <arttu.ahlroth@gmail.com>
2020-07-20 13:39:34 +0300

    cateringform update

--- Client/Site/src/Components/cateringForm.js
+++ Client/Site/src/Components/cateringForm.js
@@ -2,9 +2,10 @@ import React, {useState} from 'react'
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
-import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css"
 
 
 const CateringForm =  () => {
@@ -13,58 +14,65 @@ const CateringForm =  () => {
     return(
 
 <Container>
+
 <Col className="border d-flex justify-content-center">
 
 <Form>
-<Row>
+
+<Form.Row>
+<Col>
+<h3>Tilaisuuden tiedot</h3>
+<p>
+  Voit varata pitopalvelun alla olevalla lomakkeella.
+  loorem ipsumia lalalallaalala
+</p>
+</Col>
+</Form.Row>
+
+<Form.Row>
+  <Col>
     <Form.Label>Kommentti:</Form.Label>
     <Form.Control as="textarea" placeholder="Vapaata tekstiä"/>
-</Row>
+  </Col>
+</Form.Row>
 
 
-<Row>
-<Form.Group>
-<Form.Label>Etunimi:</Form.Label>
-  <Form.Control type="text" placeholder="ETUNIMI" />
-</Form.Group>
+<Form.Row>
+<Col>
+<Form.Label>Nimi:</Form.Label>
+  <Form.Control type="text" placeholder="Matti Meikäläinen" />
+</Col>
+</Form.Row>
 
-<Form.Group>
-<Form.Label>Sukunimi:</Form.Label>
-  <Form.Control type="text" placeholder="SUKUNIMI" />
-</Form.Group>
-</Row>
-
-<Row>
-<Form.Group>
+<Form.Row>
+<Col>
     <Form.Label>Sähköposti:</Form.Label>
-    <Form.Control type="email" placeholder="name@example.com" />
-</Form.Group>
+    <Form.Control type="email" placeholder="nimi@esimerkki.com" />
+</Col>
 
-<Form.Group>
+<Col>
     <Form.Label>Puhelinnumero:</Form.Label>
-    <Form.Control type="text" placeholder="PUHELINNUMERO" />
-</Form.Group>
-</Row>
+    <Form.Control type="text" placeholder=" Esimerkki 040 123 1234" />
+</Col>
+</Form.Row>
 
-<Row>
-  <Form.Group>
+<Form.Row>
+  <Col>
     <Form.Label>Paikka:</Form.Label>
     <Form.Control as="select">
       <option> </option>
       <option>Lilja 100 + 50 hlö</option>
       <option>sample 20 hlö</option>
     </Form.Control>
-    </Form.Group>
-    
-    <Form.Group>
-    <DatePicker
-      selected={startDate}
-      onChange={date => setStartDate(date)}
-      isClearable
-      placeholderText="I have been cleared!"
-    />
-  </Form.Group>
-</Row>
+  </Col>
+  </Form.Row>
+  <Form.Row>
+  <Col>
+    <DatePicker selected={startDate}
+    onChange={date => setStartDate(date)}
+    showWeekNumbers/>
+  </Col>
+  </Form.Row>
 
 </Form>
 </Col>
