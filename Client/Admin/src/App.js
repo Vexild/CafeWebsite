0bb87e0d5524d76f2ade18e427e6e1c0716c8d77
jyrimy <jyrimy@gmail.com>
2020-07-21 09:05:14 +0300

    Routing and further setupping

--- Client/Admin/src/App.js
+++ Client/Admin/src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import ProductList from "./Components/productList"
+import TagList from "./Components/tagList"
+import ContactInformation from "./Components/contactInformation"
 import './App.css';
 
 function App() {
+
+  const [view, setView] = useState("main");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Control Panel</h1>
       </header>
+
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/tags">Tags</Link>
+        <Link to="/contact-information"></Link>
+        <Link to="/media"></Link>
+      </nav>
+
+      <Switch>
+        <Route path="/products">
+          <ProductList />
+        </Route>
+        <Route path="/tags">
+          <TagList />
+        </Route>
+        <Route path="/contact-information">
+          <ContactInformation />
+        </Route>
+        <Route path="/media">
+
+        </Route>
+        <Route path="/">
+          <p>Oisko vaikka documentation tässä vakio näkymässä</p>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ Client/Admin/src/Components/contactInformation.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const ContactInformation = () => {
+    return (
+        <div>
+            <h3>Contact information</h3>
+        </div>
+    )
+}
+
+export default ContactInformation;
