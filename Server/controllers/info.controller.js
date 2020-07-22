d62cf92cbe7a2b69e21e88ad816d1736e21cffcb
M.K <azmoka@gmail.com>
2020-07-22 13:44:30 +0259

    Router/controller cleanup

--- Server/controllers/info.controller.js
+++ Server/controllers/info.controller.js
@@ -1,28 +1,7 @@
 import Info from '../models/info.model.js'
 
 export default {
-    newinfo: async (req, res) => {
-    let info = await Info.find({})
-        
-        if (info) {
-        res.send(`
-        <form method="POST" enctype="multipart/form-data" action="/api/info/edit">
-            <input type="text" name="address" placeholder="Address" value="${info[0].address}" required autofocus/>
-            <br/><input type="text" name="city" placeholder="City" value="${info[0].city}" required />
-            <br/><input type="text" name="phone" placeholder="Phone" value="${info[0].phone}" required/>
-            <br/><input type="text" name="email" placeholder="Email" value="${info[0].email}" required/>
-            <input type="hidden" name="id" value="${info[0]._id}"/>
-            <br/><input type="Submit" value="Save" />
-        </form>
-        `)
-    }
-    else {
-        res.send("Kissa")
-    }
-               },
-    editinfo: async (req, res) => {
-        console.log("Api/editinfo")
-        console.log(req, req.body, req.params, req.data)
+        editinfo: async (req, res) => {
         const info = await Info.findOne({})
 
         await info.updateOne(
@@ -33,7 +12,7 @@ export default {
                email: req.body.email 
             }})
             await info.save()
-        res.send("Kurwo")
+        res.send("Success.")
     },
     get: async (req, res) => {
     const data = await Info.find()
--- Server/routers/info.router.js
+++ Server/routers/info.router.js
@@ -1,14 +1,9 @@
 import express from 'express'
-import multer from 'multer'
-
-const upload = multer()
-
 import infoController from '../controllers/info.controller.js'
 
 const route = express.Router()
 
 route.get('/api/info/get', infoController.get)
-route.get('/api/info/put/contact_information', infoController.newinfo)
-route.post('/api/info/edit', upload.fields([]), infoController.editinfo)
+route.post('/api/info/edit', infoController.editinfo)
 
 export default route
