c074e4da24957bf45d3ed8b507f61b516be490b4
birghit <arttu.ahlroth@gmail.com>
2020-07-22 14:59:47 +0300

    cateringform datan consoleloggausta

--- Client/Site/src/Components/cateringForm.js
+++ Client/Site/src/Components/cateringForm.js
@@ -11,55 +11,90 @@ import "react-datepicker/dist/react-datepicker.css"
 const CateringForm =  () => {
 
     const [startDate, setStartDate] = useState(new Date());
+    const [comment, setComment] = useState('')
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phonenumber, setPhonenumber] = useState('')
+    const [location, setLocation] = useState('')
+
+    const handleSetComment = (event) => {
+      // console.log(event.target.value)
+      setComment(event.target.value)
+    }
+
+     const handleSetName = (event) => {
+      // console.log(event.target.value)
+      setName(event.target.value)
+    }
+
+    const handleSetEmail = (event) => {
+      // console.log(event.target.value)
+      setEmail(event.target.value)
+    }
+
+    const handleSetPhonenumber = (event) => {
+      // console.log(event.target.value)
+      setPhonenumber(event.target.value)
+    }
+
+    const handleSetLocation = (event) => {
+      // console.log(event.target.value)
+      setLocation(event.target.value)
+    }
+
+    const submitForm = (event) => {
+      event.preventDefault();
+      console.log(comment, name, email, phonenumber, location, startDate.toDateString())
+    }
+
     return(
 
 <Container>
 
 <Col className="d-flex justify-content-center">
 
-<Form>
+<Form onSubmit = {submitForm}>
 
 <Form.Row>
-<Col>
-<h3>Tilaisuuden tiedot</h3>
-<p>
-  Voit varata pitopalvelun alla olevalla lomakkeella.
-  loorem ipsumia lalalallaalala
-</p>
-</Col>
+  <Col>
+    <h3>Tilaisuuden tiedot</h3>
+    <p>
+      Voit varata pitopalvelun alla olevalla lomakkeella.
+      loorem ipsumia lalalallaalala
+    </p>
+  </Col>
 </Form.Row>
 
 <Form.Row>
   <Col>
     <Form.Label>Kommentti:</Form.Label>
-    <Form.Control as="textarea" placeholder="Vapaata tekstiä"/>
+    <Form.Control as="textarea" placeholder="Vapaata tekstiä" onChange={handleSetComment} required/>
   </Col>
 </Form.Row>
 
-
 <Form.Row>
-<Col>
-<Form.Label>Nimi:</Form.Label>
-  <Form.Control type="text" placeholder="Matti Meikäläinen" />
-</Col>
+  <Col>
+    <Form.Label>Nimi:</Form.Label>
+    <Form.Control type="text" placeholder="Matti Meikäläinen" onChange={handleSetName} required/>
+  </Col>
 </Form.Row>
 
 <Form.Row>
-<Col>
+  <Col>
     <Form.Label>Sähköposti:</Form.Label>
-    <Form.Control type="email" placeholder="nimi@esimerkki.com" />
-</Col>
+    <Form.Control type="email" placeholder="nimi@esimerkki.com" onChange={handleSetEmail} required/>
+  </Col>
 
-<Col>
+  <Col>
     <Form.Label>Puhelinnumero:</Form.Label>
-    <Form.Control type="text" placeholder=" Esimerkki 040 123 1234" />
-</Col>
+    <Form.Control type="number" placeholder=" Esimerkki 0401231234" onChange={handleSetPhonenumber} required/>
+  </Col>
 </Form.Row>
 
 <Form.Row>
   <Col>
     <Form.Label>Paikka:</Form.Label>
-    <Form.Control as="select">
+    <Form.Control as="select" onChange={handleSetLocation} required>
       <option> </option>
       <option>Lilja 100 + 50 hlö</option>
       <option>sample 20 hlö</option>
@@ -67,18 +102,18 @@ const CateringForm =  () => {
   </Col>
     
   <Col>
-  <Form.Label>Päivämäärä:</Form.Label>
+    <Form.Label>Päivämäärä:</Form.Label>
     <DatePicker selected={startDate}
     onChange={date => setStartDate(date)}
-    showWeekNumbers/>
+    showWeekNumbers required/>
   </Col>
 </Form.Row>
 
 <Form.Row>
   <Col>
-<Button type="submit">
-    Lähetä
-</Button>
+    <Button type="submit">
+      Lähetä
+    </Button>
   </Col>
 </Form.Row>
 
