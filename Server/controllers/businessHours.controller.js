7bdab742a6cee638fbdc0d06a272bd6c68fe2d4f
azmoka <azmoka@gmail.com>
2020-07-24 13:06:46 +0300

    /api/businesshours/put endpoint

--- Server/controllers/businessHours.controller.js
+++ Server/controllers/businessHours.controller.js
@@ -2,8 +2,16 @@ import BusinessHours from '../models/businessHours.model.js'
 
 export default {
         get: async (req, res) => {
-            console.log("tissi")
         const content = await BusinessHours.find({})
         res.send(content)
     },
+    put: async (req, res) => {
+        console.log(req.body.content)
+       BusinessHours.updateOne(
+           {},
+          {content: req.body.content }
+       )
+       .catch(error => res.send(error))
+       res.send("test")
+    }
 }
--- Server/routers/businessHours.router.js
+++ Server/routers/businessHours.router.js
@@ -4,5 +4,5 @@ import businessHoursController from '../controllers/businessHours.controller.js'
 const route = express.Router()
 
 route.get('/api/businesshours/get', businessHoursController.get)
-
+route.put('/api/businesshours/put', businessHoursController.put)
 export default route
