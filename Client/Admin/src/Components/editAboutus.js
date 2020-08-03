f40a6c36f4f4040c0bf60e95d00f3cff4c945699
birghit <arttu.ahlroth@gmail.com>
2020-08-03 10:37:13 +0300

    work in progress..

--- Client/Admin/src/Components/editAboutus.js
+++ Client/Admin/src/Components/editAboutus.js
@@ -2,16 +2,46 @@ import React, {useState} from 'react';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import ReactHtmlParser from "html-react-parser";
-
+import axios from "axios";
 
 const EditAboutus = () => {
-  const [value,setvalue] = useState("") 
+  const [value,setValue] = useState("") 
 
   const handleOnChange = (e,editor) => {
     //   console.log(editor.getData());
     const data = editor.getData()
-    setvalue(data)
+    setValue(data)
   }
+
+//   const changeHours = (event) => {
+//     event.preventDefault();
+
+//     if (content) {
+//       axios
+//         .put('http://localhost:4000/api/aboutus/put', {
+//           content: content
+//         })
+//         .then((response) => {
+//           console.log(response);
+//         })
+//         .catch((error) => console.log(error));
+//     } else {
+//       console.log("purizu wuraito samuzing");
+//     }
+//   };
+
+  const getHours = () => {
+    return axios
+      .get(`http://localhost:4000/api/aboutus/get`)
+      .then((response) => {
+        let parsedBSON;
+        parsedBSON = JSON.parse(JSON.stringify(response.data));
+        setValue(parsedBSON);
+        return parsedBSON;
+      })
+      .catch((error) => console.log(error));
+  };
+
     return (
         <div>
             <h1>EDITORI</h1>
