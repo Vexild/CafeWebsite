ffa24ef8e4e10eb6648b10073325cd64568216c0
birghit <arttu.ahlroth@gmail.com>
2020-08-19 12:32:45 +0300

    taglist alotus

---
+++ Client/Admin/src/Components/tag.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tag = (props) => {
+  return (
+    <div>
+      <p>{props.data.name}</p>
+    </div>
+  )
+}
+
+export default Tag;
--- Client/Admin/src/Components/tagList.js
+++ Client/Admin/src/Components/tagList.js
@@ -1,11 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+import Tag from './tag';
 
 const TagList = () => {
+  const [tags, setTags] = useState();
+
+  const getTags = () => {
+    return axios
+      .get(`http://localhost:4000/api/tags/get`)
+      .then((response) => {
+        let parsedBSON;
+        parsedBSON = JSON.parse(JSON.stringify(response.data));
+        console.log(parsedBSON);
+        setTags(parsedBSON);
+        return parsedBSON;
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const arrayIntoComponents = () => {
+    return tags.map((data, key) => <Tag data={data} key={key} /> )
+  }
+
+  if (!tags) {
+    getTags();
+    return (
+      <div>
+        <h3>Tags loading...</h3>
+      </div>
+    );
+  } else {
     return (
         <div>
-            <h2>Tag list</h2>
+        <h2>Tag list</h2>
+        {console.log(tags, "hulapaloo")}
+        {arrayIntoComponents()}
         </div>
-    )
-}
+    );
+  }
+};
 
 export default TagList;
