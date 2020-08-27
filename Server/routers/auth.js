118088c1eefd8ca33e2819ffd1ccb5e07b6fc061
M.K <azmoka@gmail.com>
2020-08-27 15:30:43 +0259

    dur

--- Server/routers/auth.js
+++ Server/routers/auth.js
@@ -3,9 +3,11 @@ import jwt from 'jsonwebtoken'
  const auth = async (req, res) => {
      console.log("Auth")
      if (!req.header('cookie')) {
+         console.log("No cookie")
             res.sendStatus(403)
         }
         else {
+            console.log("Cookie: ", req.header('cookie'))
         const token = req.header('cookie').split('=')[1]
         const authorizedData = "asd"
         const tempSecret = "asdfghj"
